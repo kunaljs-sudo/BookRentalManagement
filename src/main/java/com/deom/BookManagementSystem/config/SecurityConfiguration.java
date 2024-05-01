ab08953aa0c204099e3a2a9b03df63207a35a92b
Kunal Jaiswal <jkunal366@gmail.com>
2024-05-01 17:42:51 +0530

    Made changes for Swagger to work

--- src/main/java/com/deom/BookManagementSystem/config/SecurityConfiguration.java
+++ src/main/java/com/deom/BookManagementSystem/config/SecurityConfiguration.java
@@ -28,10 +28,16 @@ public class SecurityConfiguration {
         }
 
         private static final String[] WHITE_LIST_URL = { "/auth/**",
+                        "/v2/api-docs",
+                        "/v3/api-docs",
+                        "/v3/api-docs/**",
+                        "/swagger-resources",
+                        "/swagger-resources/**",
+                        "/configuration/ui",
+                        "/configuration/security",
                         "/swagger-ui/**",
-                        "/swagger-ui.html",
-                        "/swagger-ui/index.html",
-                        "/swagger-ui/index.html/**" };
+                        "/webjars/**",
+                        "/swagger-ui.html" };
 
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
